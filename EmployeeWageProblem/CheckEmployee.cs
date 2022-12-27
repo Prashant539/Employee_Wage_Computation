07a2098b5c4a5f92552021431e9f85ce2e4f61c7
Prashant <prashant.chaurasiya.cp@gmail.com>
2022-12-27 15:42:40 +0530

    [Prashant]EmployeeCheck: Present or Absent

---
+++ EmployeeWageProblem/CheckEmployee.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWageProblem
+{
+    public class CheckEmployee
+    {
+        public void CheckAttendence()
+        {
+            int Present = 1;
+            Random random = new Random();
+            int empCheck = random.Next(0, 2);
+            if (empCheck == Present)
+            {
+                Console.WriteLine("Employee is Present");
+
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent");
+
+            }
+
+        }
+
+    }
+}
+    
+
---
+++ EmployeeWageProblem/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace EmployeeWageProblem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Employee Computation Problem");
+
+            CheckEmployee checkEmployee = new CheckEmployee();
+            checkEmployee.CheckAttendence();
+        }
+    }
+}
